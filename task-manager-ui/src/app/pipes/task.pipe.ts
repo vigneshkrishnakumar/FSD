596594c31c86d0ff27a38ef182c0e6f0ae463a05
Vignesh Krishnakumar <Vignesh.K2@cognizant.com>
2019-04-16 17:37:55 +0530

    Task Manager UI

---
+++ task-manager-ui/src/app/pipes/task.pipe.ts
@@ -0,0 +1,68 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'taskfilter'
+})
+export class TaskPipe implements PipeTransform {
+
+    transform(value: Array<any>, 
+        taskName?: string,
+        parentTask?: string,
+        priorityFrom?: string,
+        priorityTo?: string,
+        startDate?: string,
+        endDate?: string) {
+        console.log('Task Pipe Value', value);
+        console.log('Task Pipe Args', taskName);
+        console.log('Task Pipe Args', parentTask);
+        console.log('Task Pipe Args', startDate);
+        console.log('Task Pipe Args', priorityFrom);
+        console.log('Task Pipe Args', priorityTo);
+
+        if(taskName.trim() == '' && 
+        parentTask.trim() == '' && 
+        priorityFrom.trim() == '' && 
+        priorityTo.trim() == '' && 
+        startDate.trim() == '' && 
+        endDate.trim() == ''){
+            return value;
+        }
+
+        if(taskName.trim() != '') {
+            let filteredTasks = value.filter(val => {
+                return val.newTask.taskName.includes(taskName)
+            });
+            return filteredTasks
+        } 
+        if(parentTask.trim() != '') {
+            let filteredTasks = value.filter(val => {
+                return val.parentTask.includes(parentTask)
+            });
+            return filteredTasks
+        }
+        if(startDate.trim() != '') {
+            let filteredTasks = value.filter(val => {
+                return val.newTask.startDate.includes(startDate)
+            });
+            return filteredTasks
+        }
+        if(endDate.trim() != '') {
+            let filteredTasks = value.filter(val => {
+                return val.newTask.endDate.includes(endDate)
+            });
+            return filteredTasks
+        }
+        if(priorityFrom.trim() != '' && priorityTo.trim() != '') {
+            let priorityTasks = [];
+            for(let val in value) {
+                console.log('Priority From '+value[val].newTask.priority);
+                if(value[val].newTask.priority >=priorityFrom && value[val].newTask.priority <=priorityTo) {
+                    priorityTasks.push(value[val]);
+                }
+            }
+            console.log('Final '+priorityTasks);
+            return priorityTasks
+        }
+    }
+
+}
